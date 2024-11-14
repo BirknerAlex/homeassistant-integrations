0a9157e17d8fabde6c009365a4eb11efe802e62e
Alexander Birkner <alexander.birkner@gpcore.io>
2024-11-15 00:30:11 +0100

    Renaming

--- custom_components/einskomma5grad/const.py
+++ custom_components/einskomma5grad/const.py
@@ -1,6 +1,6 @@
 """Constants for 1KOMMA5GRAD integration."""
 
-DATA_HASS_CONFIG = "1k5g_hass_config"
-DOMAIN = "1k5g"
+DATA_HASS_CONFIG = "einskomma5grad_hass_config"
+DOMAIN = "einskomma5grad"
 MANUFACTURER = "1KOMMA5GRAD"
-LOGGER_NAME = "1k5g"
+LOGGER_NAME = "einskomma5grad"
